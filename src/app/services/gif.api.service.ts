648c0d549505258c7c4ce610fb2f72b67ee6f6be
DeveloperMateo <mateo212705@gmail.com>
2024-03-23 12:56:32 -0500

    complete render

--- src/app/services/gif.api.service.ts
+++ src/app/services/gif.api.service.ts
@@ -13,7 +13,7 @@ export class GifApiRestService {
   constructor(private httpClient: HttpClient) { }
 
   findGifs(findTerm: string): Observable<any> {
-    const url = `${this.apiUrl}?api_key=${this.apiKey}&q=${findTerm}&limit=20`;
+    const url = `${this.apiUrl}?api_key=${this.apiKey}&q=${findTerm}&limit=21`;
     return this.httpClient.get(url);
   }
 }
